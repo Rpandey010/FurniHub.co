fb066dae95fc2589cc7701b5a41a541501a6780a
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-05-05 12:16:16 +0530

    Update Product.jsx

--- client/src/Pages/Product.jsx
+++ client/src/Pages/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import Breadcrums from '../Components/Breadcrums/Breadcrums'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
-import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+// import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../Context/ShopContext'
@@ -14,7 +14,7 @@ const Product = () => {
     <div>
       <Breadcrums product={product}/>
       <ProductDisplay product={product}/>
-      <DescriptionBox/>
+      {/* <DescriptionBox/> */}
       <RelatedProducts/>
     </div>
   )
