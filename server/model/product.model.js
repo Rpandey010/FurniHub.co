e682a2f3ffde053da93a138da2168cb6192b5d67
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-05-05 12:20:04 +0530

    Update product.model.js

--- server/model/product.model.js
+++ server/model/product.model.js
@@ -1,40 +1,70 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    id: {
-      type: Number,
-      required: true,
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number
+  },
+  old_price: {
+    type: Number
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  width: {
+    type: Number,
+    required: true,
+  },
+  length: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+  emailID: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\S+@\S+\.\S+/.test(v);
+      },
+      message: props => `${props.value} is not a valid email!`
     },
-    name: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    new_price: {
-      type: Number
-    },
-    old_price: {
-      type: Number
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    avilable: {
-      type: Boolean,
-      default: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-  });
+  },
+  sellerName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
 
-  module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Product', ProductSchema);
