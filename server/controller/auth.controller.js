1c2566d09974a76c40a942ce5c44e11e710a7646
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-05-06 16:35:13 +0530

    Update auth.controller.js

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,24 +1,44 @@
-// auth.controller.js
-const express = require("express");
 const jwt = require("jsonwebtoken");
 const Users = require('../model/user.model');
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBvKxi2XnnbDc8V8NaSMeFyiAc3jFpBEKo",
+        authDomain: "furnihub-52977.firebaseapp.com",
+        projectId: "furnihub-52977",
+        storageBucket: "furnihub-52977.appspot.com",
+        messagingSenderId: "719022156889",
+        appId: "1:719022156889:web:d62dd324ccb99ccbedd548",
+        measurementId: "G-91QP5YJH38"
+    });
+}
+
+if (!admin.apps.length) {
+    const serviceAccount = require('../furnihub-52977-firebase-adminsdk-qf5i9-bc6404b042.json');
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+}
 
 // Middleware to fetch user from database
 const fetchUser = async (req, res, next) => {
-  const token = req.header("auth-token");
-  if (!token) {
-    return res.status(401).json({ errors: "Please authenticate using a valid token" });
-  }
-  try {
-    const data = jwt.verify(token, process.env.JWT_SECRET || "secret_ecom");
-    req.user = data.user;
-    next();
-  } catch (error) {
-    return res.status(401).json({ errors: "Invalid or expired token" });
-  }
+    const token = req.header("auth-token");
+    if (!token) {
+        return res.status(401).json({ errors: "Please authenticate using a valid token" });
+    }
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET || "secret_ecom");
+        req.user = data.user;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ errors: "Invalid token" });
+    }
 };
 
-exports.login = async (req, res) => {
+const login = async (req, res) => {
     console.log("Login");
     try {
         const user = await Users.findOne({ email: req.body.email });
@@ -37,7 +57,7 @@ exports.login = async (req, res) => {
     }
 };
 
-exports.signup = async (req, res) => {
+const signup = async (req, res) => {
     console.log("Sign Up");
     try {
         const check = await Users.findOne({ email: req.body.email });
@@ -59,3 +79,24 @@ exports.signup = async (req, res) => {
         res.status(500).json({ success: false, errors: "Internal Server Error" });
     }
 };
+
+const googleLogin = async (req, res) => {
+    const { idToken, username } = req.body; // Assuming the username is sent along with the request
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        const { uid, email } = decodedToken;
+        let user = await Users.findOne({ uid: uid });
+        if (!user) {
+            user = new Users({ uid: uid, email: email, username: username });
+            await user.save();
+        }
+        return res.json({ success: true, uid: uid, email: email, username: username });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+
+
+module.exports = { fetchUser, login, signup, googleLogin }; // Exporting all functions together
