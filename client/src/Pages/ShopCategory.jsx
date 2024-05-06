037e5b5c058f004c55b5b634d19b92acc45b65ed
Zilean12 <aryan.sharma21@st.niituniversity.in>
2024-05-06 17:05:54 +0530

    added sort by features

--- client/src/Pages/ShopCategory.jsx
+++ client/src/Pages/ShopCategory.jsx
@@ -7,38 +7,58 @@ import { Link } from "react-router-dom";
 const ShopCategory = (props) => {
 
   const [allproducts, setAllProducts] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc'); // Default sort order is ascending
 
   const fetchInfo = () => { 
     fetch('http://localhost:4000/products/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
-    }
+  }
 
-    useEffect(() => {
-      fetchInfo();
-    }, [])
-    
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  }
+
+  const sortedProducts = [...allproducts].filter(item => props.category === item.category);
+
+  // Sort products based on price
+  if (sortOrder === 'asc') {
+    sortedProducts.sort((a, b) => a.new_price - b.new_price);
+  } else {
+    sortedProducts.sort((a, b) => b.new_price - a.new_price);
+  }
+
   return (
     <div className="shopcategory">
       <img src={props.banner} className="shopcategory-banner" alt="" />
       <div className="shopcategory-indexSort">
-        <p><span>Showing 1 - 12</span> out of 54 Products</p>
-        <div className="shopcategory-sort">Sort by  <img src={dropdown_icon} alt="" /></div>
+        <p><span>Showing 1 - {sortedProducts.length}</span> out of {allproducts.length} Products</p>
+        <div className="shopcategory-sort">
+          Sort by 
+          <select value={sortOrder} onChange={handleSortChange}>
+            <option value="asc">Low to High</option>
+            <option value="desc">High to Low</option>
+          </select>
+        </div>
       </div>
       <div className="shopcategory-products">
-        {allproducts.map((item,i) => {
-            if(props.category===item.category)
-            {
-              return <Item id={item.id} key={i} name={item.name} image={item.image}  new_price={item.new_price} old_price={item.old_price}/>;
-            }
-            else
-            {
-              return null;
-            }
-        })}
+        {sortedProducts.map((item, i) => (
+          <Item 
+            id={item.id} 
+            key={i} 
+            name={item.name} 
+            image={item.image}  
+            new_price={item.new_price} 
+            old_price={item.old_price}
+          />
+        ))}
       </div>
       <div className="shopcategory-loadmore">
-      <Link to='/' style={{ textDecoration: 'none' }}>Explore More</Link>
+        <Link to='/' style={{ textDecoration: 'none' }}>Explore More</Link>
       </div>
     </div>
   );
